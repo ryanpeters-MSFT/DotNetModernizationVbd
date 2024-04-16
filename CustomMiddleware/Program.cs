50606a45dc206160350b10a42c9263f55ca40e59
Ryan Peters <ryanpeters@microsoft.com>
2024-04-16 08:21:48 -0400

    add .Use() example

--- CustomMiddleware/Program.cs
+++ CustomMiddleware/Program.cs
@@ -7,8 +7,22 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
+// option 1: use middleware class
 app.UseMiddleware<FormatRequestKeyMiddleware>();
 app.UseMiddleware<ForbidTestApiKeysMiddleware>();
+
+// option 2: use .Use(...)
+app.Use(async (context, next) =>
+{
+    context.Response.OnStarting(() =>
+    {
+        context.Response.Headers.Append("hello", "I'm from the middleware");
+
+        return Task.CompletedTask;
+    });
+
+    await next(context);
+});
 
 app.MapControllers();
 
